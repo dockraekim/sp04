a5d267f365a83e5cc27096fe60fb26783dda07a9
- <-@192.168.80.43>
2021-02-22 11:03:23 +0900

    ---커밋 2020.2.22

---
+++ sp04/bin/src/main/webapp/resources/admin/js/layout.js
@@ -0,0 +1,185 @@
+
+
+$(document).ready(function(){
+	
+	// 쿠키 생성
+	function setCookie(cName, cValue, cDay){
+		var expire = new Date();
+		expire.setDate(expire.getDate() + cDay);
+		cookies = cName + '=' + escape(cValue) + '; path=/ '; // 한글 깨짐을 막기위해 escape(cValue)를 합니다.
+		if(typeof cDay != 'undefined') cookies += ';expires=' + expire.toGMTString() + ';';
+		document.cookie = cookies;
+	}
+
+	// 쿠키 가져오기
+	function getCookie(cName) {
+		cName = cName + '=';
+		var cookieData = document.cookie;
+		var start = cookieData.indexOf(cName);
+		var cValue = '';
+		if(start != -1){
+			start += cName.length;
+			var end = cookieData.indexOf(';', start);
+			if(end == -1)end = cookieData.length;
+			cValue = cookieData.substring(start, end);
+		}
+		return unescape(cValue);
+	}
+    
+	//화면 줄였다 늘렸다하기
+	$(".wild").click(function() { //최상단 오른쪽 bars(전체메뉴/사이트맵) 메뉴
+		if( 	getCookie('width')!="wild" ){
+				$(".wild img").attr("src","img/hrm/ico-0-small.png");
+				$("#wrap").addClass("active");
+				setCookie('width', 'wild', 1);
+		}
+		else if(getCookie('width')=="wild" ){
+			$(".wild img").attr("src","img/hrm/ico-0-wild.png");
+			$("#wrap").removeClass("active");
+			setCookie('width', '', -1);
+		}
+	});
+	
+	if(getCookie('width')=="wild"){
+		$(".wild img").attr("src","img/hrm/ico-0-small.png");
+		$("#wrap").addClass("active");
+	}
+	
+	
+	//파일업로드 관련
+   var fileTarget = $('.filebox .upload-hidden');
+
+    fileTarget.on('change', function(){
+        if(window.FileReader){
+            // 파일명 추출
+            var filename = $(this)[0].files[0].name;
+        } 
+
+        else {
+            // Old IE 파일명 추출
+            var filename = $(this).val().split('/').pop().split('\\').pop();
+        };
+
+        $(this).siblings(' .upload-name').val(filename);
+
+    });
+
+    //파일업로드시 이미지 보기
+    var imgTarget = $('.preview-image .upload-hidden');
+
+    imgTarget.on('change', function(){
+        var parent = $(this).parent();
+        parent.children('.upload-display').remove();
+
+        if(window.FileReader){
+            //image 파일만
+            if (!$(this)[0].files[0].type.match(/image\//)) return;
+            
+            var reader = new FileReader();
+            reader.onload = function(e){
+                var src = e.target.result;
+                parent.prepend('<span style="background-image: url('+src+');" class="thum"></span>');
+                
+            }
+            reader.readAsDataURL($(this)[0].files[0]);
+        }
+
+        else {
+            $(this)[0].select();
+            $(this)[0].blur();
+            var imgSrc = document.selection.createRange().text;
+            parent.prepend('<div class="upload-display"><div class="upload-thumb-wrap"><img class="upload-thumb"></div></div>');
+
+            var img = $(this).siblings('.upload-display').find('img');
+            img[0].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enable='true',sizingMethod='scale',src=\""+imgSrc+"\")";        
+        }
+    });
+
+    
+    //달력달력 한페이지에 4개까지 가능
+    $(function() {
+	  $( "#datepicker1" ).datepicker({
+	    dateFormat: 'yy.mm.dd',
+	    prevText: '이전 달',
+	    nextText: '다음 달',
+	    monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+	    monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+	    dayNames: ['일','월','화','수','목','금','토'],
+	    dayNamesShort: ['일','월','화','수','목','금','토'],
+	    dayNamesMin: ['일','월','화','수','목','금','토'],
+	    showMonthAfterYear: true,
+	    changeMonth: true,
+	    changeYear: true,
+	    yearSuffix: '년'
+	  });
+	  $( "#datepicker2" ).datepicker({
+	    dateFormat: 'yy.mm.dd',
+	    prevText: '이전 달',
+	    nextText: '다음 달',
+	    monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+	    monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+	    dayNames: ['일','월','화','수','목','금','토'],
+	    dayNamesShort: ['일','월','화','수','목','금','토'],
+	    dayNamesMin: ['일','월','화','수','목','금','토'],
+	    showMonthAfterYear: true,
+	    changeMonth: true,
+	    changeYear: true,
+	    yearSuffix: '년'
+	  });
+	  $( "#datepicker3" ).datepicker({
+	    dateFormat: 'yy.mm.dd',
+	    prevText: '이전 달',
+	    nextText: '다음 달',
+	    monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+	    monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+	    dayNames: ['일','월','화','수','목','금','토'],
+	    dayNamesShort: ['일','월','화','수','목','금','토'],
+	    dayNamesMin: ['일','월','화','수','목','금','토'],
+	    showMonthAfterYear: true,
+	    changeMonth: true,
+	    changeYear: true,
+	    yearSuffix: '년'
+	  });
+	  $( "#datepicker4" ).datepicker({
+	    dateFormat: 'yy.mm.dd',
+	    prevText: '이전 달',
+	    nextText: '다음 달',
+	    monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+	    monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+	    dayNames: ['일','월','화','수','목','금','토'],
+	    dayNamesShort: ['일','월','화','수','목','금','토'],
+	    dayNamesMin: ['일','월','화','수','목','금','토'],
+	    showMonthAfterYear: true,
+	    changeMonth: true,
+	    changeYear: true,
+	    yearSuffix: '년'
+	  });
+	  
+	});
+    
+    
+    
+    //토글팝업
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    });
+    
+    //메뉴
+	$(".nav ul li").hover(function() { 
+		var navId = $(this).attr("data-id");
+		$("#"+navId).show();
+		
+	},function() { 
+		var navId = $(this).attr("data-id");
+		$("#"+navId).hide();
+		
+	});	    
+		    
+		    
+});
+
+
+
+
+
+	
